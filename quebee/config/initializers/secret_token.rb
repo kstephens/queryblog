2d3ab57c5edf490b6d0b0282a6d6520797248ad4
Kurt Stephens <ks.github@kurtstephens.com>
2011-03-10 16:44:12 -0600

    Initial Rails 3 create.

---
+++ quebee/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quebee::Application.config.secret_token = '5c88b428c11de4cb68e131f641f5f04c83e933faedfecd636d04eccc4ab476fa3b4bc47b6020e69208c0712bb140458c01c3672c2197d350a33061675e2c47c1'

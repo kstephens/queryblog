8fefc9439bddf8588003b9dec8559e15da7dbbd0
Kurt Stephens <ks.github@kurtstephens.com>
2011-03-12 18:59:53 -0600

    Changes from "rails new quebee -m http://datamapper.org/templates/rails.rb".

--- quebee/config/initializers/secret_token.rb
+++ quebee/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Quebee::Application.config.secret_token = '5c88b428c11de4cb68e131f641f5f04c83e933faedfecd636d04eccc4ab476fa3b4bc47b6020e69208c0712bb140458c01c3672c2197d350a33061675e2c47c1'
+Quebee::Application.config.secret_token = 'be668fb6f258f91deca9841359f8fe256696e85e8caef5a6886928ac042e9e9bdae62e77073ae84c94498dad9e2e452dc71b721eaf4dd03597768abb25053e72'

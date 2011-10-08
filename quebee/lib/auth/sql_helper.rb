259faa8a59f44f2f1c102c139178f09d776c8d74
Kurt Stephens <ks.github@kurtstephens.com>
2011-10-08 01:36:28 -0500

    Debugging.

--- quebee/lib/auth/sql_helper.rb
+++ quebee/lib/auth/sql_helper.rb
@@ -8,9 +8,12 @@ module SqlHelper
     repos = repos.default_repository_name if Class === repos
     repos = repository(repos)
     repos.adapter.send(:with_connection) do |connection|
+      $stderr.puts "connection = #{connection.class}"
       begin
         command = connection.create_command(sql)
+        $stderr.puts "command = #{command.class}"
         reader = command.execute_reader(*params)
+        $stderr.puts "reader = #{reader.class}"
         # $stderr.puts "reader = #{reader.inspect}"
         columns = reader.fields.dup
         types = reader.instance_variable_get(:@field_types).dup
